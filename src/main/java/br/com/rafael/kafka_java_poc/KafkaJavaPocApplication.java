877918a608586a414ff1453c5dd9d45c76dc2545
RafaelTS <rafaeltorress@gmail.com>
2025-04-28 16:58:07 -0300

    project started

---
+++ src/main/java/br/com/rafael/kafka_java_poc/KafkaJavaPocApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafael.kafka_java_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaJavaPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaJavaPocApplication.class, args);
+	}
+
+}
